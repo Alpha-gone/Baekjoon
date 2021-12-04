e28cdf02dec151bac3c58d42ead0d8cb09c0dee8
DESKTOP\Alphagone <990131@naver.com>
2021-12-05 00:37:19 +0900

    재귀함수 끝

--- src/step10/Q11729.java
+++ src/step10/Q11729.java
@@ -6,23 +6,27 @@ import Function.*;
     
 public class Q11729 {
     static int count = 0;
+    static StringBuffer sb = new StringBuffer();
     public static void main(String[] args) throws IOException {
         int input = Cast.StoInt(IO.bInput());
 
         hanoi(input, 1, 2, 3);
+        IO.bWrite(Cast.ItoString(count) + "\n");
+        IO.bWrite(sb.toString());
         IO.EndProg();
     }
 
-    public static void hanoi(int panel, int start, int work, int target) throws IOException{
+    public static StringBuffer hanoi(int panel, int start, int mid, int target){
         count++;
 
         if(panel == 1){
-            IO.bWrite(Cast.ItoString(start) + " " + Cast.ItoString(target));
-            return;
+            sb.append(Cast.ItoString(start) + " " + Cast.ItoString(target) + "\n");
+            return sb;
         }else{
-            hanoi(panel - 1, start, target, work);
-            //IO.bWrite(Cast.ItoString(panel));
-            hanoi(panel - 1, )
+            hanoi(panel - 1, start, target, mid);
+            sb.append(Cast.ItoString(start) + " " + Cast.ItoString(target) + "\n");
+            hanoi(panel - 1, mid, start, target);
+            return sb;
         }
     } 
 }
